0aef78e96f1d8f93cc19d0afad69e94cb4d3c227
안광수 <angwangsu@angwangsuui-MacBookPro.local>
2021-04-07 22:09:12 +0900

    로그인 페이지 완성

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -18,6 +18,7 @@ class _LoginState extends State<Login> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(height: 100),
             Image.asset('assets/image/makekitchen_logo.png',width:220),
             SizedBox(height: 100.0),
             Padding(
@@ -31,11 +32,14 @@ class _LoginState extends State<Login> {
                       labelStyle: TextStyle(
                         color: Colors.black
                       ),
-                      border:OutlineInputBorder(),
+                      border:OutlineInputBorder(
+                        borderRadius: BorderRadius.zero
+                      ),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(
                           color: Colors.black
-                        )
+                        ),
+                        borderRadius: BorderRadius.zero
                       )  
                     ),
                   ),
@@ -48,14 +52,81 @@ class _LoginState extends State<Login> {
                       labelStyle: TextStyle(
                         color: Colors.black
                       ),
-                      border: OutlineInputBorder(),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.zero
+                      ),
                       focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.zero,
                         borderSide: BorderSide(
                           color: Colors.black
                         )
                       )
                     ),
-                  )
+                  ),
+                  SizedBox(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: (){
+                          print('비밀번호 찾기!');
+                        },
+                        child: Text('비밀번호를 잊어버리셨나요?',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey
+                        ),),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50,),
+                  FlatButton(
+                    onPressed: ()=>{
+                      print('로그인!')
+                    },
+                    
+                    padding: EdgeInsets.fromLTRB(50, 15, 50, 15),
+                    color: Colors.blue[700],
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                    child: Text('로그인',style: TextStyle(color: Colors.white,fontSize: 20))
+                  ),
+                  SizedBox(height: 15,),
+                  GestureDetector(
+                    onTap: ()=>{
+                      print('회원가입!')
+                    },
+                    child: Text('회원가입',
+                    style: TextStyle(fontSize: 16,color: Colors.grey),
+                    )
+                  ),
+                  SizedBox(height: 60),
+                  Text('소셜 로그인', style: TextStyle(color: Colors.grey,fontSize: 20)),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: ()=>{
+                          print('kakao Login!')
+                        },
+                        child: Image.asset('assets/image/kakao_logo.png')
+                      ),
+                      SizedBox(width: 10),
+                      GestureDetector(
+                        onTap: ()=>{
+                          print('naver Login!')
+                        },
+                        child: Image.asset('assets/image/naver_logo.png')
+                      ),
+                      SizedBox(width: 10),
+                      GestureDetector(
+                        onTap: ()=>{
+                          print('google Login!')
+                        },
+                        child: Image.asset('assets/image/google_logo.png')
+                        )
+                    ],
+                  ),
                 ],
               ),
             ),
